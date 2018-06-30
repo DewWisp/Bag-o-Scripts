15ae04f0c1c5e515f9bd9c7456664aaeaf5703cd
DewWisp <avaitorboy@gmail.com>
2018-06-29 22:49:45 -0400

    Updated elpa-clean.sh

--- elpa-clean.sh
+++ elpa-clean.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o posix
 
 if [[ -e "$HOME/.emacs.d/elpa" ]]; then
-    pushd "$HOME/.emacs.d/elpa"
+    pushd "$HOME/.emacs.d/elpa" >>/dev/null
 else
     echo -e "\e[33mError\e[0m: \e[31memacs elpa package directory not found...\e[0m\n" && exit 1
 fi
 for package in $(ls -d */ | sed 's/\///g'); do
-    if [[ ! $(grep -P '((?<=\w)(-(\d|\.)+))' <<< "$package") ]]; then
+    if [[ ! $(grep -P '((?<=\w)(-(\d|\.)+))' <<< "$package") ]] || [[ "$package" =~ (gnupg|archives) ]]; then
         continue
     else
-        mv "$package" $(echo -n "$package" | perl -ne 's/((?<=\w)(-(\d|\.)+))//g && print $_')
+        mv "$package" $(printf "$package" | perl -ne 's/((?<=\w)(-(\d|\.)+))//g && print $_')
     fi
 done
-ls -1
-popd
+printf "emacs Packages:\n"
+for item in $(ls -1 | grep -Pv '(gnupg|archives)'); do
+    printf -- "-> %s\n" $item
+done
+popd >>/dev/null
